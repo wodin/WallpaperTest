189dc7988fba9ce58ccc5da151d6f22ef5f524ec
Michael Wood <esiotrot@gmail.com>
2022-03-12 01:14:16 +0200

    Add react-native-manage-wallpaper

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, TouchableOpacity, Text } from "react-native";
+import ManageWallpaper, { TYPE } from "react-native-manage-wallpaper";
 
 export default function App() {
+
+  const _callback = (res) => {
+    console.log("Response: ", res);
+  };
+
+  const _setWallpaper = () => {
+    ManageWallpaper.setWallpaper(
+      {
+        uri: "https://i.pinimg.com/originals/76/5e/1d/765e1dc8cb1cc115fb3b0b39a895fdeb.jpg",
+      },
+      _callback,
+      TYPE.HOME
+    );
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#F5FCFF",
+      }}
+    >
+      <TouchableOpacity
+        style={{
+          paddingHorizontal: 30,
+          paddingVertical: 8,
+          marginBottom: 24,
+          borderRadius: 16,
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "#000000",
+        }}
+        onPress={_setWallpaper}
+      >
+        <Text
+          style={{
+            fontSize: 20,
+            margin: 10,
+            textAlign: "center",
+            color: "#FFFFFF",
+          }}
+        >
+          Change Home Wallpaper
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
